2e5f1542bad000caa8ca98a9026c30b3d0c2fb08
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-02-21 00:49:48 +0900

    abc192F WA

--- abc192/f.cpp
+++ abc192/f.cpp
@@ -10,19 +10,28 @@ int main() {
     ll sum = 0;
     for (int i = 0; i < n; ++i) {
         cin >> a[i];
-        sum += a[i];
     }
-    if ((x - sum) % n == 0) {
-        cout << (x - sum) / n << endl;
-        return 0;
-    }
-    vector<ll> b(n);
-    ll result = LONG_LONG_MAX;
-    for (int i = 0; i < n; ++i) {
-        b[i] = sum - a[i];
-        if ((x - b[i]) % (n-1) == 0) {
-            result = min(result, (x - b[i]) / (n - 1));
+    ll result = 0;
+    int cc = 0;
+    for (int c = 1; c <= n; ++c) {
+        ll dp[100][100][100] = {0};
+        for (int i = 0; i < n; ++i) {
+            int r = a[i] % c;
+            dp[i][0][r] = max(dp[i][0][r], a[i]);
+            for (int j = 0; j < c && j <= i; ++j) {
+                for (int k = 0; k < c; ++k) {
+                    int p = (k + r) % c;
+                    if (j > 0 && dp[i-1][j-1][k] > 0) {
+                        dp[i][j][p] = max(dp[i][j][p], dp[i-1][j-1][k] + a[i]);
+                    }
+                    dp[i][j][p] = max(dp[i][j][p], dp[i-1][j][p]);
+                }
+            }
+        }
+        if (dp[n-1][c-1][x % c] > result) {
+            result = dp[n-1][c-1][x % c];
+            cc = c;
         }
     }
-    cout << result << endl;
+    cout << (x - result) / cc << endl;
 }
