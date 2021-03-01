12d912e8a5a0f269f31460c62c2d7900695e96f9
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-03-01 12:20:19 +0900

    arc032B

---
+++ arc032/b.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007;
+
+class UnionFind {
+private:
+    vector<int> parent;
+    vector<ll> size;
+public:
+    UnionFind(unsigned long n): parent(n), size(n, 1) {
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+    }
+
+    ll count(int x) {
+        return size[root(x)];
+    }
+
+    int root(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = root(parent[x]);
+    }
+
+    void unite(int x, int y) {
+        x = root(x);
+        y = root(y);
+        if (x == y) return;
+
+        if (size[x] > size[y]) {
+            parent[y] = x;
+            size[x] += size[y];
+        } else {
+            parent[x] = y;
+            size[y] += size[x];
+        }
+    }
+
+    bool same(int x, int y) {
+        return root(x) == root(y);
+    }
+};
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    UnionFind uf(n);
+    for (int i = 0; i < m; ++i) {
+        int a, b;
+        cin >> a >> b;
+        a--; b--;
+        uf.unite(a, b);
+    }
+    set<int> par;
+    for (int i = 0; i < n; ++i) {
+        par.insert(uf.root(i));
+    }
+    cout << par.size() - 1 << endl;
+}
