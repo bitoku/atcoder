01d018f38569377e1f4d2bbf1cccd4273dc964c4
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-03-19 11:54:12 +0900

    abc159C

---
+++ abc159/c.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007;
+
+int main() {
+    ll l;
+    cin >> l;
+    cout << setprecision(15) << l * l * l / 27.0 << endl;
+}
