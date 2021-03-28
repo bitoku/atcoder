870f29515f95e88d007c64cd6f8c8c1b5bb12fc6
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-03-29 01:07:34 +0900

    abc168E

--- abc168/e.cpp
+++ abc168/e.cpp
@@ -1,69 +1,67 @@
-#include <iostream>
-#include <set>
-#include <map>
-
+#include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+typedef long double ld;
+const ll mod = 1000000007;
 
-const int MOD = 1e9 + 7;
-
-long long gcd(long long a, long long b) {
-    if(a < b) return gcd(b, a);
-    long long r;
-    while ((r=a%b)) {
-        a = b;
-        b = r;
-    }
-    return b;
+template<class T>
+T gcd(T a, T b) {
+    if (a < b) swap(a, b);
+    if (b == 0) return a;
+    return gcd(b, a%b);
 }
 
-long long modpow(long long a, long long n, long long mod) {
-    long long res = 1;
-    while (n > 0) {
-        if (n & 1) res = res * a % mod;
-        a = a * a % mod;
+template< typename T >
+T mod_pow(T x, T n, const T &p) {
+    T ret = 1;
+    while(n > 0) {
+        if(n & 1) (ret *= x) %= p;
+        (x *= x) %= p;
         n >>= 1;
     }
-    return res;
+    return ret;
 }
 
 int main() {
-    long long n;
-    map<long long, pair<long long, pair<long long, long long>>> status;
-    map<long long, set<long long>> match;
-    multiset<pair<long long, pair<long long, long long>>> statusset;
+    ll n;
     cin >> n;
-
-    for (long long i=0; i<n; i++) {
-        long long a, b, g;
-        long long sign = 1;
+    map<tuple<int, ll, ll>, ll> m;
+    for (int i = 0; i < n; ++i) {
+        ll a, b;
         cin >> a >> b;
-        if (a < 0) {
-            a = -a;
-            sign *= -1;
+        int sign = 1;
+        if (a == 0 && b != 0) {
+            m[make_tuple(0, 0, 1)]++;
+            continue;
         }
-        if (b < 0) {
-            b = -b;
-            sign *= -1;
+        if (b == 0 && a != 0) {
+            m[make_tuple(0, 1, 0)]++;
+            continue;
         }
-        g = gcd(a, b);
-        a = a / g;
-        b = b / g;
-        status[i] = make_pair(sign, make_pair(a, b));
-        cout << sign << ' ' <<  a << ' ' << b << endl;
-        statusset.insert(make_pair(sign, make_pair(a, b)));
+        if (a == 0 && b == 0) {
+            m[make_tuple(0, 0, 0)]++;
+            continue;
+        }
+        if ((a < 0 && b > 0) || (a > 0 && b < 0)) sign = -1;
+        a = abs(a);
+        b = abs(b);
+        ll g = gcd(a, b);
+        m[make_tuple(sign, a / g, b / g)]++;
     }
-
-    long long result = 0;
-    for (long long i=0; i<n; i++) {
-        long long sign = status[i].first;
-        long long a = status[i].second.first;
-        long long b = status[i].second.second;
-        unsigned long cnt = (statusset.size()) - statusset.count(make_pair(-sign, make_pair(b, a))) - 1;
-        cout << cnt << endl;
-        result = (result + modpow(2, cnt, MOD)) % MOD;
-        statusset.erase(status[i]);
+    ll result = 1;
+    set<tuple<int, ll, ll>> s;
+    for (auto [t, x]: m) {
+        auto [sign, a, b] = t;
+        if (a == 0 && b == 0) continue;
+        auto tt = make_tuple(-sign, b, a);
+        if (s.find(tt) != s.end()) continue;
+        s.insert(t);
+        result *= (mod_pow(2ll, m[t], mod) + mod_pow(2ll, m[tt], mod) - 1 + mod) % mod;
+        result %= mod;
     }
-
+    result--;
+    result += m[make_tuple(0, 0, 0)];
+    result += mod;
+    result %= mod;
     cout << result << endl;
-    return 0;
 }
