75aef27971a63287292f00a37ec158290204d6fb
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-04-03 16:23:08 +0100

    abc128E

--- abc128/e.cpp
+++ abc128/e.cpp
@@ -13,27 +13,48 @@ struct stop{
 int main() {
     ll n, q;
     cin >> n >> q;
-    vector<stop> stops;
-    map<ll, ll> road;
+    vector<stop> stops(n);
+    map<ll, vector<ll>> road;
     for (int i = 0; i < n; ++i) {
         cin >> stops[i].s >> stops[i].t >> stops[i].x;
-        road[stops[i].s] = LONG_LONG_MAX;
-        road[stops[i].t] = LONG_LONG_MAX;
+        road[stops[i].s - stops[i].x].push_back(stops[i].x);
+        road[stops[i].t - stops[i].x].push_back(-stops[i].x);
     }
-    vector<ll> d(q);
-    for (int i = 0; i < q; ++i) {
-        cin >> d[i];
-        road[d[i]] = LONG_LONG_MAX;
-    }
-    multiset<int> s;
-    for (auto& [a, b] : d) {
-        if (b > 0) {
-            s.insert(b);
-        } else {
-            auto it = s.find(-b);
-            s.erase(it);
+    multiset<ll> s;
+    map<ll, ll> roadmin;
+    for (auto [k, vx]: road) {
+        for (long long x : vx) {
+            if (x > 0) {
+                s.insert(x);
+                if (s.empty()) {
+                    roadmin[k] = LONG_LONG_MAX;
+                } else {
+                    roadmin[k] = *(s.begin());
+                }
+            } else {
+                auto it = s.find(-x);
+                s.erase(it);
+                if (s.empty()) {
+                    roadmin[k] = LONG_LONG_MAX;
+                } else {
+                    roadmin[k] = *(s.begin());
+                }
+            }
         }
-        b = *min_element(s.begin(), s.end());
     }
-
+    for (int i = 0; i < q; ++i) {
+        ll d;
+        cin >> d;
+        auto it = roadmin.upper_bound(d);
+        if (it == roadmin.begin()) {
+            cout << -1 << endl;
+        } else {
+            it--;
+            if ((*it).second == LONG_LONG_MAX) {
+                cout << -1 << endl;
+            } else {
+                cout << (*it).second << endl;
+            }
+        }
+    }
 }
