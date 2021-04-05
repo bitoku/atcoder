4006b21e96fca90a74e8c9948a18cf6dc7f493f8
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-04-05 12:21:31 +0100

    arc099C

---
+++ arc099/c.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const ll mod = 1000000007;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    cout << (n - 1 + k - 2) / (k - 1) << endl;
+}
