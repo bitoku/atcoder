d13ac2734910c6c665de3e8e3a89c336d0d75546
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-04-18 10:00:36 +0100

    code-thanks-festival-2017-openD

---
+++ code-thanks-festival-2017-open/d.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const ll mod = 1000000007;
+
+template<class T>
+T gcd(T a, T b) {
+    if (a < b) swap(a, b);
+    if (b == 0) return a;
+    return gcd(b, a%b);
+}
+
+int main() {
+    ll n, m;
+    cin >> n >> m;
+    ll g = gcd(n, m);
+    cout << m - g << endl;
+}
