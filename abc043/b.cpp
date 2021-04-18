d4e3852110cb035bda83eaaeb5d8d92be7f75099
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-04-18 17:06:28 +0100

    abc043B

---
+++ abc043/b.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const ll mod = 1000000007;
+
+int main() {
+    string s;
+    cin >> s;
+    string t;
+    for (int i = 0; i < s.size(); ++i) {
+        if (s[i] == '0') {
+            t.push_back('0');
+        } else if (s[i] == '1') {
+            t.push_back('1');
+        } else {
+            t.pop_back();
+        }
+    }
+    cout << t << endl;
+}
