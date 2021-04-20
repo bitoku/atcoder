83a4aa55c878e8e5afd83d26c32e94866a4cd8f2
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-04-20 19:29:53 +0100

    abc049B

---
+++ abc049/b.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const ll mod = 1000000007;
+
+int main() {
+    ll h, w;
+    cin >> h >> w;
+    vector<string> s(h);
+    for (int i = 0; i < h; ++i) {
+        cin >> s[i];
+        cout << s[i] << endl;
+        cout << s[i] << endl;
+    }
+}
