341dd046aadf2c02afb62c035338ba911e47b7e7
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-04-21 17:21:51 +0100

    agc052A

---
+++ agc052/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const ll mod = 1000000007;
+
+int main() {
+    ll t;
+    cin >> t;
+    for (int i = 0; i < t; ++i) {
+        ll n;
+        cin >> n;
+        string s;
+        cin >> s;
+        cin >> s;
+        cin >> s;
+        cout << string(n, '1') << string(n, '0') << '1' << endl;
+    }
+}
