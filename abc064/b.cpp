26442b50725dfc3cc4c27c2876aff17916e08fb9
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-05-01 23:17:12 +0100

    abc064B

---
+++ abc064/b.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const ll mod = 1000000007;
+
+int main() {
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    cout << *max_element(a.begin(), a.end()) - *min_element(a.begin(), a.end()) << endl;
+}
