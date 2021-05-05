a46b6d1edc1be01047cbcf8a36bc483c7aa4d12f
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-05-05 20:41:54 +0100

    abc156E

--- abc156/e.cpp
+++ abc156/e.cpp
@@ -1,37 +1,55 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-const int MOD = 1e9 + 7;
-int fact[200001] = {0};
+typedef long double ld;
+const ll mod = 1000000007;
 
-ll mpow(ll x, int n) {
-    ll ans = 1;
-    while (n > 0) {
-        if (n & 1) ans = (ans * x) % MOD;
-        x = (x * x) % MOD;
+template< typename T >
+T mod_pow(T x, T n) {
+    T ret = 1;
+    while(n > 0) {
+        if(n & 1) (ret *= x) %= mod;
+        (x *= x) %= mod;
         n >>= 1;
     }
-    return ans;
+    return ret;
 }
 
-ll combi(int n, int r){
-    ll ans = (fact[n] * mpow(fact[n-r], MOD-2)) % MOD;
-    ans = ans * mpow(fact[r], MOD-2) % MOD;
-    return ans;
+unordered_map<ll, ll> fact_memo;
+ll mod_fact(ll x) {
+    if (fact_memo.find(x) != fact_memo.end()) return fact_memo[x];
+    if (x == 0) return 1;
+    fact_memo[x] = x * mod_fact(x - 1) % mod;
+    return fact_memo[x];
+}
+
+void init_fact(ll x) {
+    for (int i = 0; i < x; ++i) {
+        mod_fact(i);
+    }
+}
+
+unordered_map<ll, ll> invfact_memo;
+ll mod_invfact(ll x) {
+    if (invfact_memo.find(x) != invfact_memo.end()) return invfact_memo[x];
+    invfact_memo[x] = mod_pow(mod_fact(x), mod - 2);
+    return invfact_memo[x];
+}
+
+ll mod_combi(ll n, ll r) {
+    if (r < 0 || n < r) return 0;
+    return mod_fact(n) * mod_invfact(r) % mod * (mod_invfact(n - r)) % mod;
 }
 
 int main() {
-    int n, k;
+    ll n, k;
     cin >> n >> k;
-    fact[0] = 1;
-    for (ll i = 1; i <= 200000;i++) {
-        fact[i] = (fact[i-1] * i) % MOD;
+    ll result = 0;
+    init_fact(n);
+    for (int i = 0; i <= min(k, n-1); ++i) {
+        // nCi * (n-1)Ci
+        result += mod_combi(n, i) * mod_combi(n-1, i) % mod;
+        result %= mod;
     }
-    ll l = min(k, n - 1);
-    ll ans = 0;
-    for(int i = 1; i <= l; ++i){
-        ans += ((combi(n - 1, i) * combi(n, i))) % MOD;
-    }
-    cout << (ans+1) % MOD << endl;
+    cout << result << endl;
 }
