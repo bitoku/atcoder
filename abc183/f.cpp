71ff95ebc39594b69e9e01f8001ba098d077f00d
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-05-06 20:44:27 +0100

    abc095D

---
+++ abc183/f.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const ll mod = 1000000007;
+
+class UnionFind {
+private:
+    vector<int> parent;
+    vector<ll> size;
+public:
+    UnionFind(unsigned long n): parent(n), size(n, 1) {
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+    }
+
+    ll count(int x) {
+        return size[root(x)];
+    }
+
+    int root(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = root(parent[x]);
+    }
+
+    void unite(int x, int y) {
+        x = root(x);
+        y = root(y);
+        if (x == y) return;
+
+        if (size[x] > size[y]) {
+            parent[y] = x;
+            size[x] += size[y];
+        } else {
+            parent[x] = y;
+            size[y] += size[x];
+        }
+    }
+
+    bool same(int x, int y) {
+        return root(x) == root(y);
+    }
+};
+
+
+int main() {
+    ll n, q;
+    cin >> n >> q;
+    vector<ll> c(n);
+}
