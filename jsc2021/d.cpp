04d8fd280d9bd8c78149aaf747b9f7c3fb453345
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-05-27 22:18:05 +0100

    jsc2021D

---
+++ jsc2021/d.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const ll mod = 1000000007;
+
+template< typename T >
+T mod_pow(T x, T n, const T &p) {
+    T ret = 1;
+    while(n > 0) {
+        if(n & 1) (ret *= x) %= p;
+        (x *= x) %= p;
+        n >>= 1;
+    }
+    return ret;
+}
+
+int main() {
+    ll n, p;
+    cin >> n >> p;
+    ll result = p-1;
+    result *= mod_pow(p - 2, n - 1, mod);
+    cout << result % mod << endl;
+}
