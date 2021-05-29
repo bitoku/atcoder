71689ee33d4f6a6d5714f5b79e6567dd267a870a
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-05-29 20:51:27 +0100

    arc118B

--- arc118/b.cpp
+++ arc118/b.cpp
@@ -4,6 +4,8 @@ typedef long long ll;
 typedef long double ld;
 const ll mod = 1000000007;
 
+// TODO: solve without editorial
+
 template<class Condition>
 ll binsearch(ll ng, ll ok, Condition func) {
     while (abs(ok - ng) > 1) {
