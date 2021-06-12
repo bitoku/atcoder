06c70fd5a9a56ddfd9928e3765a6c63fe36b1057
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-06-12 17:17:41 +0100

    abc161E

---
+++ abc161/e.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const ll mod = 1000000007;
+
+int main() {
+    ll n, k, c;
+    string s;
+    cin >> n >> k >> c;
+    cin >> s;
+    vector<ll> l(k);
+    vector<ll> r(k);
+    int x = INT_MIN;
+    int j = 0;
+    for (int i = 0; i < n; ++i) {
+        if (x + c < i && s[i] == 'o') {
+            l[j++] = i;
+            x = i;
+            if (j == k) break;
+        }
+    }
+    x = INT_MAX;
+    j = k - 1;
+    for (int i = n - 1; i >= 0; --i) {
+        if (x - c > i && s[i] == 'o') {
+            r[j--] = i;
+            x = i;
+            if (j == -1) break;
+        }
+    }
+    for (int i = 0; i < k; ++i) {
+        if (l[i] == r[i]) cout << l[i]+1 << endl;
+    }
+}
