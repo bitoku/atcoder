6ead2909d22fd2b42b81d88a44bd76e1b3fbd867
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-06-22 11:02:21 +0100

    abc206E

--- abc206/e.cpp
+++ abc206/e.cpp
@@ -4,35 +4,26 @@ typedef long long ll;
 typedef long double ld;
 const ll mod = 1000000007;
 
-ll eulerPhi(ll n) {
-    if (n == 0) return 0;
-    ll result = n;
-    for (ll x = 2; x * x <= n; ++x) {
-        if (n % x == 0) {
-            result -= result / x;
-            while (n % x == 0) n /= x;
-        }
-    }
-    if (n > 1) result -= result / n;
-    return result;
-}
+// TODO: solve without editorial
 
 int main() {
     ll l, r;
     cin >> l >> r;
-    cout << eulerPhi(1) << endl;
-    vector<ll> euler(r / 2 + 1);
-    for (int i = 2; i <= r / 2; ++i) {
-        euler[i] = eulerPhi(i);
+    vector<ll> x(r+1);
+    vector<ll> x2(r+1);
+    ll result = 0;
+    for (int i = r; i >= 2; --i) {
+        x[i] = r / i - (l - 1) / i;
     }
-    for (int i = 1; i <= r / 2; ++i) {
-        euler[i] += euler[i-1];
+    for (int i = r; i >= 2; --i) {
+        x2[i] = x[i] * x[i];
+        for (int j = 2 * i; j <= r; j += i) {
+            x2[i] -= x2[j];
+        }
+        result += x2[i];
     }
-    for (int i = 1; i <= r / 2; ++i) {
-        euler[i] *= 2;
+    for (int i = max(2ll, l); i <= r; ++i) {
+        result -= 2 * x[i] - 1;
     }
-    for (int i = 2; i < r; ++i) {
-        ll sup = r / i;
-
-    }
+    cout << result << endl;
 }
