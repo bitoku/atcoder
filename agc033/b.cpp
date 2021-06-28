2a744ca6ebaf1a65c985dd7b6714f691ace92699
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-06-28 22:46:42 +0100

    agc033B

--- agc033/b.cpp
+++ agc033/b.cpp
@@ -1,49 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef long double ld;
 const ll mod = 1000000007;
 
-// TODO: solve without editorial
+// TODO: 06/28
 
 int main() {
-    ll H, W, N;
+    int H, W, N;
     cin >> H >> W >> N;
-    ll sh, sw;
-    cin >> sh >> sw;
-    string S, T;
-    cin >> S;
-    cin >> T;
-    ll down = 1, up = H;
-    for (ll i = N - 1; i >= 0; --i) {
-        if (T[i] == 'D') down = max(1ll, down-1);
-        if (T[i] == 'U') up = min(up+1, H);
-        if (S[i] == 'D') up--;
-        if (S[i] == 'U') down++;
-        if (down > up) {
+    int sy, sx;
+    cin >> sy >> sx;
+    string s, t;
+    cin >> s;
+    cin >> t;
+    int left = 1, right = W;
+    int bottom = H, top = 1;
+    for (int i = N - 1; i >= 0; --i) {
+        if (t[i] == 'L') {
+            right = min(W, right+1);
+        }
+        if (t[i] == 'R') {
+            left = max(1, left-1);
+        }
+        if (t[i] == 'U') {
+            bottom = min(H, bottom+1);
+        }
+        if (t[i] == 'D') {
+            top = max(1, top - 1);
+        }
+
+        if (s[i] == 'L') {
+            left++;
+        }
+        if (s[i] == 'R') {
+            right--;
+        }
+        if (s[i] == 'U') {
+            top++;
+        }
+        if (s[i] == 'D') {
+            bottom--;
+        }
+        if (left > right || bottom < top) {
             cout << "NO" << endl;
             return 0;
         }
     }
-    if (sh < down || up < sh) {
+    if (left <= sx && sx <= right && top <= sy && sy <= bottom) {
+        cout << "YES" << endl;
+    } else {
         cout << "NO" << endl;
-        return 0;
     }
-    ll left = 1, right = W;
-    for (ll i = N - 1; i >= 0; --i) {
-        if (T[i] == 'R') left = max(1ll, left-1);
-        if (T[i] == 'L') right = min(right+1, W);
-        if (S[i] == 'R') right--;
-        if (S[i] == 'L') left++;
-        if (left > right) {
-            cout << "NO" << endl;
-            return 0;
-        }
-    }
-    if (sw < left || right < sw) {
-        cout << "NO" << endl;
-        return 0;
-    }
-
-
-    cout << "YES" << endl;
 }
