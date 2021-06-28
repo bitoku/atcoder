00f1b13e729be4c906ac8115f6e03231c83e37b2
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-06-28 01:03:35 +0100

    agc031B

--- agc031/b.cpp
+++ agc031/b.cpp
@@ -1,28 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef long double ld;
 const ll mod = 1000000007;
 
-// TODO: solve without editorial
+// TODO: 6/28
 
 int main() {
-    int n;
+    ll n;
     cin >> n;
-    vector<ll> dp(n+1);
-    vector<ll> color(200010);
-    dp[0] = 1;
-    int before = -1;
+    vector<ll> c(n);
     for (int i = 0; i < n; ++i) {
-        int c;
-        cin >> c;
-        c--;
-        if (before != c) {
-            dp[i+1] = (dp[i] + color[c]) % mod;
-            color[c] = (color[c] + dp[i]) % mod;
+        cin >> c[i];
+    }
+    vector<ll> dp(n+1);
+    vector<ll> color(200001, -1);
+    dp[0] = 1;
+    for (int i = 0; i < n; ++i) {
+        if (color[c[i]] == i - 1 || color[c[i]] == -1) {
+            dp[i+1] = dp[i] % mod;
         } else {
-            dp[i+1] = dp[i];
+            dp[i+1] = (dp[i] + dp[color[c[i]] + 1]) % mod;
         }
-        before = c;
+        color[c[i]] = i;
     }
     cout << dp[n] << endl;
 }
