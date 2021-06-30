71979e6893192793b40f62cdda98dfd108cc00d2
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-06-30 22:18:28 +0100

    abc207E WIP

--- abc207/e.cpp
+++ abc207/e.cpp
@@ -8,27 +8,32 @@ int main() {
     ll n;
     cin >> n;
     vector<ll> a(n);
-    vector<vector<ll>> m(n + 1, vector<ll>(n + 1));
+    vector<vector<ll>> dp(n, vector<ll>(n+1));
+    for (int i = 0; i < n; ++i) {
+        dp[i][1] = 1;
+    }
     for (int i = 0; i < n; ++i) {
         cin >> a[i];
     }
+    vector<ll> sum(n+1);
     for (int i = 0; i < n; ++i) {
-        for (int j = 2; j <= n; ++j) {
-            m[j][i] = a[i] % j;
-        }
+        sum[i+1] = sum[i] + a[i];
     }
-    for (int j = 2; j <= n; ++j) {
-        for (int i = 1; i <= n; ++i) {
-            m[j][i] += m[j][i-1];
-        }
-    }
-    vector<vector<ll>> dp(n+1, vector<ll>(n));
+    vector<vector<ll>> x(n+1, vector<ll>(n));
     for (int i = 0; i < n; ++i) {
-        dp[1][i] = 1;
-    }
-    for (int i = 2; i <= n; ++i) {
-        for (int j = i - 1; j < n; ++j) {
-            dp[i][j] = dp[i-1][j];
+        for (int j = 0; j < i; ++j) {
+            x[i+1][sum[j] % (i + 1)] += dp[j][i];
+            x[i+1][sum[j] % (i + 1)] %= mod;
+        }
+        if (i == 0) continue;
+        for (int j = 0; j < n; ++j) {
+            dp[j][i] = x[i][sum[j] % i];
         }
     }
+    ll result = 0;
+    for (int i = 1; i <= n; ++i) {
+        result += dp[n-1][i];
+        result %= mod;
+    }
+    cout << result << endl;
 }
