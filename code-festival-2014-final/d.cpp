93b68f688d0d656df4d091137fd57055d384cf96
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-07-04 15:53:34 +0100

    code-festival-2014-finalD

---
+++ code-festival-2014-final/d.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const ll mod = 1000000007;
+
+int main() {
+    ll a;
+    cin >> a;
+    cout << a + 1 << ' ' << 2 << endl;
+}
