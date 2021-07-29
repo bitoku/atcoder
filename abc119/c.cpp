3fc4a2d911ae6d5f7ab275c454314e3374933c2f
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-07-29 21:02:12 +0100

    abc119C

--- abc119/c.cpp
+++ abc119/c.cpp
@@ -1,31 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef long double ld;
 const ll mod = 1000000007;
 
-// TODO: solve without editorial
-int a, b, c;
+// SOLVED: 07/29
 
-ll dfs(int aa, int bb, int cc, const vector<int>& l, int i) {
-    if (i == l.size()) {
-        if (min({aa, bb, cc}) == 0) return INT_MAX;
-        return abs(aa - a) + abs(bb - b) + abs(cc - c) - 30;
+template< typename T >
+T mod_pow(T x, T n, const T &p) {
+    T ret = 1;
+    while(n > 0) {
+        if(n & 1) (ret *= x) %= p;
+        (x *= x) %= p;
+        n >>= 1;
     }
-    return min({
-                       dfs(aa + l[i], bb, cc, l, i + 1) + 10,
-                       dfs(aa, bb + l[i], cc, l, i + 1) + 10,
-                       dfs(aa, bb, cc + l[i], l, i + 1) + 10,
-                       dfs(aa, bb, cc, l, i + 1),
-    });
+    return ret;
 }
 
 int main() {
-    int n;
+    ll n;
     cin >> n;
-    cin >> a >> b >> c;
-    vector<int> l(n);
+    ll a[3];
+    cin >> a[0] >> a[1] >> a[2];
+    vector<ll> l(n);
     for (int i = 0; i < n; ++i) {
         cin >> l[i];
     }
-    cout << dfs(0, 0, 0, l, 0) << endl;
+    ll lim = 1;
+    for (int i = 0; i < n; ++i) {
+        lim *= 4;
+    }
+    lim--;
+    ll result = LONG_LONG_MAX;
+    for (int i = 0; i < lim; ++i) {
+        ll temp = 0;
+        ll cnt = 0;
+        ll x[3] = {};
+        for (ll j = 0; j < n; ++j) {
+            ll k = i / mod_pow(4ll, j, LONG_LONG_MAX) % 4;
+            if (k == 0) continue;
+            x[k - 1] += l[j];
+            cnt++;
+        }
+        if (x[0] == 0 || x[1] == 0 || x[2] == 0) continue;
+        temp += abs(a[0] - x[0]) + abs(a[1] - x[1]) + abs(a[2] - x[2]);
+        temp += (cnt - 3) * 10;
+        result = min(result, temp);
+    }
+    cout << result << endl;
 }
