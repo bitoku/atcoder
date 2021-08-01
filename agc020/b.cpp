01c2fc4abfb91f362736192864b1fe7951c77845
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-08-01 22:40:34 +0100

    agc020B

--- agc020/b.cpp
+++ agc020/b.cpp
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef long double ld;
 const ll mod = 1000000007;
 
-// TODO: solve without editorial
+// TODO: 08/01
 
 int main() {
-    int k;
+    ll k;
     cin >> k;
     vector<ll> a(k);
     for (int i = 0; i < k; ++i) {
@@ -16,14 +17,17 @@ int main() {
         cout << -1 << endl;
         return 0;
     }
-    ll l = 2, r = 3;
-    for (int i = k - 2; i >= 0; --i) {
-        l = (l + a[i] - 1) / a[i] * a[i];
-        r = r / a[i] * a[i] + a[i] - 1;
-        if (l > r) {
+    reverse(a.begin(), a.end());
+    ll l = 2, r = 2;
+    for (int i = 0; i < k; ++i) {
+        ll x = (l + a[i] - 1) / a[i] * a[i];
+        ll y = (r / a[i]) * a[i];
+        if (x < l || r < x || y < l || r < y) {
             cout << -1 << endl;
             return 0;
         }
+        l = x;
+        r = y + a[i] - 1;
     }
     cout << l << ' ' << r << endl;
 }
