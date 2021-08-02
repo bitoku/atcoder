c36b5ee3b933133b1c49e031e6e04e7622608cab
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-08-02 20:38:28 +0100

    abc144E

--- abc144/e.cpp
+++ abc144/e.cpp
@@ -1,13 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef long double ld;
 const ll mod = 1000000007;
-
-// TODO: solve without editorial
 
 template<class Condition>
 ll binsearch(ll ng, ll ok, Condition func) {
-    // ngは常に条件を満たさない, okは常に条件を満たす
     while (abs(ok - ng) > 1) {
         ll mid = (ng + ok) / 2;
         if (func(mid)) ok = mid;
@@ -27,16 +25,20 @@ int main() {
     for (int i = 0; i < n; ++i) {
         cin >> f[i];
     }
-    sort(a.begin(), a.end(), greater<>());
     sort(f.begin(), f.end());
-    auto func = [a, f, k, n](ll x) {
-        ll temp = k;
+    sort(a.begin(), a.end());
+    auto func = [&](ll x) {
+        ll temp = 0;
         for (int i = 0; i < n; ++i) {
-            temp -= max(a[i] - x / f[i], 0ll);
-            if (temp < 0) return false;
+            ll t = x / f[i];
+            if (a[n-i-1] > t) {
+                temp += a[n-i-1] - t;
+            }
+            if (temp > k) {
+                return false;
+            }
         }
         return true;
     };
-    cout << binsearch(-1, 1ll << 40, func) << endl;
+    cout << binsearch(-1, 1e13, func) << endl;
 }
-
