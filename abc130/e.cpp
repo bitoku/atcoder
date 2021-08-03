210b95d1b3386278e3a9782cb35710ca2eadca35
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-08-03 20:23:57 +0100

    abc130E

--- abc130/e.cpp
+++ abc130/e.cpp
@@ -4,37 +4,31 @@ typedef long long ll;
 typedef long double ld;
 const ll mod = 1000000007;
 
-// TODO: solve without editorial
-
 int main() {
     int n, m;
     cin >> n >> m;
-    vector<vector<ll>> dp(n+1, vector<ll>(m+1));
-    vector<vector<ll>> sum(n+1, vector<ll>(m+1));
-    vector<ll> s(n);
-    vector<ll> t(m);
+    vector<ll> s(n), t(m);
     for (int i = 0; i < n; ++i) {
         cin >> s[i];
     }
     for (int i = 0; i < m; ++i) {
         cin >> t[i];
     }
+    vector<vector<ll>> dp(n+1, vector<ll>(m+1));
+    for (int i = 0; i <= n; ++i) {
+        dp[i][0] = 1;
+    }
+    for (int i = 0; i <= m; ++i) {
+        dp[0][i] = 1;
+    }
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
-            if (s[i] == t[j]) {
-                dp[i+1][j+1] = (sum[i][j] + 1) % mod;
-            } else {
-                dp[i+1][j+1] = 0;
+            dp[i+1][j+1] = dp[i+1][j] + dp[i][j+1];
+            if (s[i] != t[j]) {
+                dp[i + 1][j + 1] -= dp[i][j];
             }
-            sum[i+1][j+1] = (sum[i+1][j] + sum[i][j+1] - sum[i][j] + dp[i+1][j+1] + mod) % mod;
+            dp[i+1][j+1] = (dp[i+1][j+1] + mod) % mod;
         }
     }
-    ll result = 0;
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= m; ++j) {
-            result += dp[i][j];
-            result %= mod;
-        }
-    }
-    cout << result + 1 << endl;
+    cout << dp[n][m] << endl;
 }
