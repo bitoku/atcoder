780360e3416bfee65aafca1e88a7f40c76e85658
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-08-16 21:36:39 +0100

    typical9064

--- typical90/64.cpp
+++ typical90/64.cpp
@@ -7,32 +7,29 @@ const ll mod = 1000000007;
 int main() {
     ll n, q;
     cin >> n >> q;
-    vector<ll> a(2 * n);
-    for (int i = 0; i < 2 * n; i += 2) {
+    vector<ll> a(n);
+    for (int i = 0; i < n; ++i) {
         cin >> a[i];
-        a[i+1] = a[i];
     }
     ll sum = 0;
-    for (int i = 1; i < 2 * n - 1; i += 2) {
-        sum += abs(a[i+1] - a[i]);
+    vector<ll> b(n-1);
+    for (int i = 0; i < n-1; ++i) {
+        b[i] = a[i+1] - a[i];
+        sum += abs(b[i]);
     }
     for (int i = 0; i < q; ++i) {
         ll l, r, v;
         cin >> l >> r >> v;
         l--; r--;
         if (l > 0) {
-            sum -= abs(a[2 * l] - a[2 * l - 1]);
+            sum -= abs(b[l-1]);
+            b[l-1] += v;
+            sum += abs(b[l-1]);
         }
         if (r < n - 1) {
-            sum -= abs(a[2 * r + 2] - a[2 * r + 1]);
-        }
-        a[2 * l] += v;
-        a[2 * r + 1] += v;
-        if (l > 0) {
-            sum += abs(a[2 * l] - a[2 * l - 1]);
-        }
-        if (r < n - 1) {
-            sum += abs(a[2 * r + 2] - a[2 * r + 1]);
+            sum -= abs(b[r]);
+            b[r] -= v;
+            sum += abs(b[r]);
         }
         cout << sum << endl;
     }
