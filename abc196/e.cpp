51148268076a59379af5b2ba88dd18f7a47c6e51
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-08-23 22:17:12 +0100

    abc196E

--- abc196/e.cpp
+++ abc196/e.cpp
@@ -4,33 +4,29 @@ typedef long long ll;
 typedef long double ld;
 const ll mod = 1000000007;
 
-// TODO: solve without editorial
+// SOLVED: 08/23
 
 int main() {
-    int n, q;
+    ll n;
     cin >> n;
-    ll s = 0;
-    ll mn = LONG_LONG_MAX / 2;
-    ll mx = LONG_LONG_MIN / 2;
+    ll a = LONG_LONG_MAX, b = LONG_LONG_MIN, c = 0;
     for (int i = 0; i < n; ++i) {
-        ll a, t;
-        cin >> a >> t;
+        ll s, t;
+        cin >> s >> t;
         if (t == 1) {
-            s += a;
-            mn += a;
-            mx += a;
+            c += s;
         } else if (t == 2) {
-            mx = max(mx, a);
-            mn = max(mn, a);
-        } else if (t == 3) {
-            mn = min(mn, a);
-            mx = min(mx, a);
+            b = max(b, s - c);
+        } else {
+            b = min(b, s - c);
+            a = min(a, s - c);
         }
     }
+    int q;
     cin >> q;
     for (int i = 0; i < q; ++i) {
         ll x;
         cin >> x;
-        cout << max(mx, min(mn, x + s)) << endl;
+        cout << max(min(x, a), b) + c << endl;
     }
 }
