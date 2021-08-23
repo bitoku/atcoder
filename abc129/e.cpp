64000f48370c27470b54e328c08d9f20a4871157
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-08-23 19:11:02 +0100

    abc129E

--- abc129/e.cpp
+++ abc129/e.cpp
@@ -4,22 +4,21 @@ typedef long long ll;
 typedef long double ld;
 const ll mod = 1000000007;
 
-// TODO: solve without editorial
-
-ll dp[2][100010];
+// TODO: 08/23
 
 int main() {
     string l;
     cin >> l;
     reverse(l.begin(), l.end());
+    vector<vector<ll>> dp(2, vector<ll>(l.size() + 1));
     dp[0][0] = 1;
-    dp[1][0] = 1;
     for (int i = 0; i < l.size(); ++i) {
-        dp[1][i+1] = 3 * dp[1][i] % mod;
         if (l[i] == '0') {
             dp[0][i+1] = dp[0][i] % mod;
+            dp[1][i+1] = (2 * dp[0][i] + 3 * dp[1][i]) % mod;
         } else {
-            dp[0][i+1] = (2 * dp[0][i] + dp[1][i]) % mod;
+            dp[0][i+1] = (3 * dp[0][i] + dp[1][i]) % mod;
+            dp[1][i+1] = 2 * dp[1][i] % mod;
         }
     }
     cout << dp[0][l.size()] << endl;
