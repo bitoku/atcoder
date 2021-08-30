0a5204740a1010637c81be98fadeee0d1014ea65
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-08-30 21:37:28 +0100

    arc109B

--- arc109/b.cpp
+++ arc109/b.cpp
@@ -4,11 +4,10 @@ typedef long long ll;
 typedef long double ld;
 const ll mod = 1000000007;
 
-// TODO: solve without editorial
+// SOLVED: 08/30
 
 template<class Condition>
 ll binsearch(ll ng, ll ok, Condition func) {
-    // ngは常に条件を満たさない, okは常に条件を満たす
     while (abs(ok - ng) > 1) {
         ll mid = (ng + ok) / 2;
         if (func(mid)) ok = mid;
@@ -20,9 +19,9 @@ ll binsearch(ll ng, ll ok, Condition func) {
 int main() {
     ll n;
     cin >> n;
-    auto func = [n](ll k) {
-        return k * (k + 1) / 2 <= n + 1;
+    auto func = [&](ll x) {
+        return x * x + x <= 2 * n + 2;
     };
-    ll k = binsearch(INT_MAX - 1, 0, func);
-    cout << n - k + 1 << endl;
+    ll k = binsearch(3000000000, 1, func);
+    cout << n + 1 - k << endl;
 }
