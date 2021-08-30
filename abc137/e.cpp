b6551089dff7f81d073d2a15d112c54b6f556ee2
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-08-30 22:21:23 +0100

    abc137E

--- abc137/e.cpp
+++ abc137/e.cpp
@@ -4,46 +4,43 @@ typedef long long ll;
 typedef long double ld;
 const ll mod = 1000000007;
 
-// TODO: solve without editorial
+// TODO: 08/30
 
 struct Edge {
     int from;
     int to;
-    ll cost;
+    ll c;
 };
 
 int main() {
     ll n, m, p;
     cin >> n >> m >> p;
-    vector<ll> dist(n+1, LONG_LONG_MAX);
     vector<Edge> edges;
     for (int i = 0; i < m; ++i) {
-        int a, b, c;
+        int a, b;
+        ll c;
         cin >> a >> b >> c;
-        edges.push_back(Edge {a, b, p - c});
+        a--; b--;
+        edges.push_back({a, b, c-p});
     }
-    dist[1] = 0;
-    for (int i = 0; i < n - 1; ++i) {
-        for (int j = 0; j < m; ++j) {
-            auto e = edges[j];
-            if (dist[e.from] == LONG_LONG_MAX) continue;
-            if ((dist[e.from] + e.cost) < dist[e.to]) {
-                dist[e.to] = dist[e.from] + e.cost;
+    vector<ll> dist(n, LONG_LONG_MIN);
+    dist[0] = 0;
+    for (int i = 0; i < n; ++i) {
+        for (auto& e: edges) {
+            if (dist[e.from] == LONG_LONG_MIN) continue;
+            if (dist[e.to] == LONG_LONG_MIN || dist[e.to] < dist[e.from] + e.c) {
+                dist[e.to] = dist[e.from] + e.c;
             }
         }
     }
-    for (int i = 0; i < n - 1; ++i) {
-        for (int j = 0; j < m; ++j) {
-            auto e = edges[j];
-            if (dist[e.from] == LONG_LONG_MAX) continue;
-            if (dist[e.from] == LONG_LONG_MIN || (dist[e.from] + e.cost) < dist[e.to]) {
-                dist[e.to] = LONG_LONG_MIN;
+    for (int i = 0; i < n; ++i) {
+        for (auto& e: edges) {
+            if (dist[e.from] == LONG_LONG_MIN) continue;
+            if (dist[e.to] == LONG_LONG_MIN || dist[e.from] == LONG_LONG_MAX || dist[e.to] < dist[e.from] + e.c) {
+                dist[e.to] = LONG_LONG_MAX;
             }
         }
     }
-    if (dist[n] != LONG_LONG_MIN) {
-         cout << max(0ll, -dist[n]) << endl;
-    } else {
-        cout << -1 << endl;
-    }
+    if (dist[n-1] == LONG_LONG_MAX) cout << -1 << endl;
+    else cout << max(0ll, dist[n-1]) << endl;
 }
