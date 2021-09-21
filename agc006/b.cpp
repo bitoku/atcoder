d9d1bff2579d4a6f3615750c4a9a555000d09543
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-09-21 21:42:56 +0100

    agc006B

--- agc006/b.cpp
+++ agc006/b.cpp
@@ -4,41 +4,40 @@ typedef long long ll;
 typedef long double ld;
 const ll mod = 1000000007;
 
-// TODO: solve without editorial
-
 int main() {
-    ll n;
-    cin >> n;
-    ll x;
-    cin >> x;
+    ll n, x;
+    cin >> n >> x;
     if (x == 1 || x == 2 * n - 1) {
         cout << "No" << endl;
         return 0;
     }
-
     cout << "Yes" << endl;
     if (n == 2) {
-        cout << 1 << endl << 2 << endl << 3 << endl;
+        cout << "1 2 3" << endl;
         return 0;
     }
+    vector<ll> t;
+    vector<ll> v;
     if (x == 2) {
-        for (int i = 5; i <= 2 * n - 1; ++i) {
-            cout << i << endl;
-            if (i - 5 == n - 3) {
-                cout << 4 << endl << 1 << endl << 2 << endl << 3 << endl;
-            }
+        v = {4, 1, 2, 3};
+        for (int i = 1; i <= 2 * n - 1; ++i) {
+            if (1 <= i && i <= 4) continue;
+            t.push_back(i);
         }
-        return 0;
-    }
-    vector<int> a;
-    for (int i = 1; i <= 2 * n - 1; ++i) {
-        if (x - 2 <= i && i <= x + 1) continue;
-        a.push_back(i);
-    }
-    for (int i = 0; i < 2 * n - 5; ++i) {
-        cout << a[i] << endl;
-        if (i == n - 3) {
-            cout << x - 2 << endl << x << endl << x + 1 << endl << x - 1 << endl;
+    } else {
+        v = {x-2, x+1, x, x-1};
+        for (int i = 1; i <= 2 * n - 1; ++i) {
+            if (x-2 <= i && i <= x+1) continue;
+            t.push_back(i);
         }
+    }
+    for (int i = 0; i < n - 3; ++i) {
+        cout << t[i] << endl;
+    }
+    for (int i = 0; i < 4; ++i) {
+        cout << v[i] << endl;
+    }
+    for (int i = n - 3; i < 2 * n - 5; ++i) {
+        cout << t[i] << endl;
     }
 }
